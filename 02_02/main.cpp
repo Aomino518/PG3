4ba9c2eedf94a0bf2857b033ff4017096022f693
Aoto Mori <k024g0051@g.neec.ac.jp>
2025-12-26 12:49:44 +0900

    02_02直し

--- 02_02/main.cpp
+++ 02_02/main.cpp
@@ -1,6 +1,4 @@
-#include <stdlib.h>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -17,15 +15,28 @@ int Recursive(int hours) {
 	return Recursive(hours - 1) * 2 - 50;
 }
 
+/// <summary>
+/// 再帰的賃金体系の累計額を返す関数
+/// </summary>
+/// <param name="hours">働いた時間</param>
+/// <returns>賃金の合計額</returns>
+int RecursiveSum(int hours) {
+	if (hours <= 1) {
+		return (100);
+	}
+
+	return RecursiveSum(hours - 1) + Recursive(hours);
+}
+
 int main() {
-	int hours = 9;
+	int hours = 8;
 	int common = 1226;
 
 	cout << "時間\t一般的\t再帰的" << endl;
 	// 計算比較
 	for (int h = 1; h <= hours; ++h) {
 		int commonResult = common * h;
-		int recursive = Recursive(h);
+		int recursive = RecursiveSum(h);
 		cout << h << "h\t" << commonResult << "\t" << recursive << endl;
 	}
 }
