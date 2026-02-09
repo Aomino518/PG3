22ebf5f83f65c4f3588798620eea9ea43bc42879
Aoto Mori <k024g0051@g.neec.ac.jp>
2026-02-09 12:20:27 +0900

    03_02修正

--- 03_02/main.cpp
+++ 03_02/main.cpp
@@ -1,38 +1,46 @@
-#include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
+class BaseMin {
+public:
+	virtual void PrintMin() = 0;
+	virtual ~BaseMin() {}
+};
+
 template <typename Type1, typename Type2>
 
-class MinFunc {
+class MinFunc : public BaseMin{
 public:
 	Type1 a;
 	Type2 b;
 
 	MinFunc(Type1 a, Type2 b) : a(a), b(b){};
 
-	auto min() {
+	auto Min() {
 		return a < b ? a : b;
+	}
+
+	void PrintMin() override {
+		cout << Min() << endl;
 	}
 };
 
 int main() {
-	MinFunc<int, int> m1(4, 6);
-	MinFunc<float, float> m2(5.0f, 2.0f);
-	MinFunc<double, int> m3(4.35, 5.32);
+	vector<unique_ptr<BaseMin>> m;
 
-	MinFunc<int, float> m4(4, 2.5f);
-	MinFunc<int, double> m5(4, 3.14);
-	MinFunc<float, double> m6(2.5f, 3.14);
+	m.push_back(make_unique<MinFunc<int, int>>(4, 6));
+	m.push_back(make_unique<MinFunc<float, float>>(5.0f, 2.0f));
+	m.push_back(make_unique<MinFunc<double, int>>(4.35, 5.32));
+	m.push_back(make_unique<MinFunc<int, float>>(4, 2.5));
+	m.push_back(make_unique<MinFunc<int, double>>(4, 3.14));
+	m.push_back(make_unique<MinFunc<float, double>>(2.5f, 3.14));
 
-	cout << m1.min() << endl;
-	cout << m2.min() << endl;
-	cout << m3.min() << endl;
-	cout << m4.min() << endl;
-	cout << m5.min() << endl;
-	cout << m6.min() << endl;
-
+	for (auto& item : m) {
+		item->PrintMin();
+	}
+	
 	return 0;
 }
